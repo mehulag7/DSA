468eefb39b1e068f150680994090b2d7ba2a1e4e
mehulag7 <mehulagarwal2221@gmail.com>
2026-01-16 14:54:41 +0530

    Time: 3 ms (91.55%) | Memory: 13.2 MB (59.24%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -18,7 +18,24 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,1e9));
-        return solve(word1,word2,n,m,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+                if(word1[i-1]==word2[j-1]){
+                dp[i][j]=min(dp[i][j],dp[i-1][j-1]);
+            }
+            else {
+                dp[i][j]=min(dp[i][j],dp[i-1][j-1]+1);
+            }
+            }
+        }
+        return dp[n][m];
     }
 };
