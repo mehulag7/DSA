9487df2791bbc1d0ef85799998eef3a95b5b31a2
mehulag7 <mehulagarwal2221@gmail.com>
2026-01-19 00:54:01 +0530

    Time: 107 ms (60.16%) | Memory: 15.4 MB (7.24%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -22,7 +22,18 @@ public:
         for (int i = 1; i <= n; i++)
             a[i] = nums[i - 1];
 
-        vector<vector<long long>> dp(n + 2, vector<long long>(n + 2, -1));
-        return solve(1, n, a, dp);
+        vector<vector<long long>> dp(n + 2, vector<long long>(n + 2, 0));
+       // return solve(1, n, a, dp);
+       for(int i=n;i>=1;i--){
+        for(int j=i;j<=n;j++){
+            long long ans = 0;
+        for (int k = i; k <= j; k++) {
+            ans = max(ans,
+                dp[i][k-1]+dp[k+1][j]+1LL * a[i - 1] * a[k] * a[j + 1]);
+        }
+        dp[i][j]=ans;
+        }
+       }
+       return dp[1][n];
     }
 };
