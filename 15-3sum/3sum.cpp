5e03a9b919e047ea2bf80dcf53fef4259c7183f0
mehulag7 <mehulagarwal2221@gmail.com>
2026-02-03 16:07:45 +0530

    Time: 51 ms (47.56%) | Memory: 29.1 MB (70.86%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,85 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        set<vector<int>> st;          // ✅ uniqueness enforced
-        map<long long,long long> mpp;
-        vector<int> pos, neg;
-
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
-            if(nums[i] > 0) pos.push_back(nums[i]);
-            if(nums[i] < 0) neg.push_back(nums[i]);
-        }
-
-        // (0,0,0)
-        if(mpp[0] > 2){
-            st.insert({0,0,0});
-        }
-
-        sort(pos.begin(), pos.end());
-        sort(neg.begin(), neg.end(), greater<int>());
-
-        // (0, x, -x)
-        if(mpp[0] > 0){
-            for(int i = 0; i < pos.size(); i++){
-                if(i > 0 && pos[i] == pos[i-1]) continue;
-                if(mpp[-pos[i]] > 0){
-                    vector<int> t = {0, pos[i], -pos[i]};
-                    sort(t.begin(), t.end());
-                    st.insert(t);
+        vector<vector<int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=n-1;
+            while(l<r){
+                long long sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
+                while(l<r && nums[r]==nums[r+1]) r--;
+                }
+                else if(sum<0) l++;
+                else{
+                    r--;
                 }
             }
         }
-
-        // (pos, neg, third)
-        for(int i = 0; i < pos.size(); i++){
-            for(int j = 0; j < neg.size(); j++){
-
-                int third = -1 * (pos[i] + neg[j]);
-
-                if(pos[i] == (-neg[j]) * 2){
-                    if(mpp[neg[j]] > 1){
-                        vector<int> t = {pos[i], neg[j], third};
-                        sort(t.begin(), t.end());
-                        st.insert(t);
-                    }
-                    break;
-                }
-                else if(pos[i] <= (-neg[j]) * 2) break;
-                else if(mpp[third] > 0){
-                    vector<int> t = {pos[i], neg[j], third};
-                    sort(t.begin(), t.end());
-                    st.insert(t);
-                }
-            }
-        }
-
-        // (neg, pos, third)
-        for(int i = 0; i < neg.size(); i++){
-            for(int j = 0; j < pos.size(); j++){
-
-                int third = -1 * (pos[j] + neg[i]);
-
-                if(-neg[i] == pos[j] * 2){
-                    if(mpp[pos[j]] > 1){
-                        vector<int> t = {pos[j], pos[j], neg[i]};
-                        sort(t.begin(), t.end());
-                        st.insert(t);
-                    }
-                    break;
-                }
-                else if(-neg[i] <= pos[j] * 2) break;
-                else if(mpp[third] > 0){
-                    vector<int> t = {pos[j], third, neg[i]};
-                    sort(t.begin(), t.end());
-                    st.insert(t);
-                }
-            }
-        }
-
-        // set → vector
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
