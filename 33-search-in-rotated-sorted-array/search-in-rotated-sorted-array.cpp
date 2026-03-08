68d9f3c778eb8571f5aa0ae7e239bb6b58148c82
mehulag7 <mehulagarwal2221@gmail.com>
2026-03-08 16:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.94%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bs(vector<int>& nums,int low,int high,int target){
+        if(low>high) return -1;
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid;
+        else{
+            return max(bs(nums,mid+1,high,target),bs(nums,low,mid-1,target));
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        return bs(nums,0,n-1,target);
+    }
+};
