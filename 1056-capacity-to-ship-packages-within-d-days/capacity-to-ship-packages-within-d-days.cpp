7760ca12471be196395a5d590d3c5a7f45f2e994
mehulag7 <mehulagarwal2221@gmail.com>
2026-03-13 01:07:17 +0530

    Time: 7 ms (90.95%) | Memory: 35 MB (54.20%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low=*max_element(weights.begin(),weights.end()),high=1e9;
+        int ans=1e9;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int count=1;
+            int sum=0;
+            for(int i=0;i<weights.size();i++){
+                if(sum+weights[i]<=mid){
+                    sum+=weights[i];
+                }
+                else{
+                    count++;
+                    sum=weights[i];
+                }
+            }
+            if(count<=days){
+                ans=min(ans,mid);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
