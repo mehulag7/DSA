f37035b9d4b04fd5610006bcd03a6bf63a7e1534
mehulag7 <mehulagarwal2221@gmail.com>
2026-03-17 15:52:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int minTime(vector<int>& nums, int k) {
+        // code here
+        int n=nums.size();
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+        while(low<=high){
+            int mid=(low+high)/2;
+            int count=1;
+            int sum=0;
+            for(int i=0;i<n;i++){
+                if(sum+nums[i]<=mid){
+                    sum+=nums[i];
+                }
+                else{
+                    sum=nums[i];
+                    count++;
+                }
+            }
+            if(count<=k){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
