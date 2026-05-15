d5ccc15378ec381813d168cb33ffd487ea59e7d2
mehulag7 <mehulagarwal2221@gmail.com>
2026-05-16 02:35:34 +0530

    Time: 17 ms (22.55%) | Memory: 17.4 MB (64.27%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,35 +9,25 @@
  */
 class Solution {
 public:
-    bool path(TreeNode* root,TreeNode* x,TreeNode* p,vector<TreeNode*>& a){
-        if(x==NULL) return false;
-       if(x==p){
-           a.push_back(x);
-           return true;
+    TreeNode* path(TreeNode* x,TreeNode* q,TreeNode* p){
+        if(x==NULL) return NULL;
+       if(x==p || x==q){
+           return x;
        }
        else{
-           if(path(root,x->left,p,a)) {
-            a.push_back(x);
-            return true;
-           }
-           else if(path(root,x->right,p,a)){
-            a.push_back(x);
-            return true;
-           }
-           else return false;
+        TreeNode* k=path(x->left,q,p);
+        TreeNode* j=path(x->right,q,p);
+        if(!k){
+            return j;
+        }
+        else{
+            if(!j) return k;
+            else return x;
+        }
        }
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> a,b;
-        path(root,root,p,a);
-        path(root,root,q,b);
-        map<TreeNode*,int> mpp;
-        for(int i=0;i<a.size();i++){
-            mpp[a[i]]++;
-        }
-        for(int i=0;i<b.size();i++){
-            if(mpp[b[i]]==1) return b[i];
-        }
-        return root;
+        TreeNode* x=path(root,p,q);
+        return x;
     }
 };
