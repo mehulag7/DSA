49fabcc7eabc7f39363963dcbe611ec6d0394c4c
mehulag7 <mehulagarwal2221@gmail.com>
2026-05-26 14:49:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,35 +1,44 @@
+class Disjoint{
+  vector<int> sz,parent;
+  public:
+       Disjoint(int n){
+           sz.resize(n+1,1);
+           parent.resize(n+1);
+           for(int i=0;i<=n;i++) parent[i]=i;
+       }
+       int findUp(int u){
+           if(parent[u]==u) return u;
+           return parent[u]=findUp(parent[u]);
+       }
+       void unionby(int x,int z){
+           int a=findUp(x);
+           int b=findUp(z);
+           if(a==b) return;
+           if(sz[a]<sz[b]){
+               sz[b]+=sz[a];
+               parent[a]=b;
+           }
+           else{
+               parent[b]=a;
+               sz[a]+=sz[b];
+           }
+       }
+};
 class Solution {
   public:
     int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
         int ans=0;
-        vector<vector<pair<int,int>>> adj(V);
+        vector<vector<int>> adj;
         for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+            adj.push_back({edges[i][2],edges[i][0],edges[i][1]});
         }
-        priority_queue<
-        pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>
-        > q;
-        q.push({0,{0,-1}});
-        vector<int> vis(V,0);
-        while(!q.empty()){
-            int wt=q.top().first;
-            int node=q.top().second.first;
-            int parent=q.top().second.second;
-            q.pop();
-            if(vis[node]) continue;
-            else{
-                vis[node]=1;
-                ans+=wt;
-            }
-            for(auto i:adj[node]){
-                if(!vis[i.first]){
-                    q.push({i.second,{i.first,node}});
-                }
-            }
+        sort(adj.begin(),adj.end());
+        Disjoint ds(V+1);
+        for(int i=0;i<adj.size();i++){
+            if(ds.findUp(adj[i][1])==ds.findUp(adj[i][2])) continue;
+            ans+=adj[i][0];
+            ds.unionby(adj[i][1],adj[i][2]);
         }
         return ans;
     }
