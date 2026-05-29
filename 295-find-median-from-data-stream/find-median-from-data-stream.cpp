6c07d72f058ffc887cc4ece7bfd0e4adea3f3c18
mehulag7 <mehulagarwal2221@gmail.com>
2026-05-29 14:28:32 +0530

    Time: 62 ms (81.20%) | Memory: 148.5 MB (62.64%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -6,23 +6,23 @@ public:
         
     }
     
-   void addNum(int num) {
-
-    if(left.empty() || num <= left.top())
-        left.push(num);
-    else
-        right.push(num);
-
-    if(left.size() > right.size() + 1) {
-        right.push(left.top());
-        left.pop();
+    void addNum(int num) {
+        if(left.empty()){
+            left.push(num);
+        }
+        else{
+            if(left.top()>=num) left.push(num);
+            else right.push(num);
+        }
+        if(left.size()>right.size()+1) {
+            right.push(left.top());
+            left.pop();
+        }
+        else if(left.size()<right.size()){
+            left.push(right.top());
+            right.pop();
+        }
     }
-
-    if(right.size() > left.size()) {
-        left.push(right.top());
-        right.pop();
-    }
-}
     
     double findMedian() {
         if(left.size()>right.size()){
