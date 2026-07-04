2f0fe2e3500671f6b7f89cbf81b130f6ae9ab43c
mehulag7 <mehulagarwal2221@gmail.com>
2026-07-04 23:15:31 +0530

    Time: 7 ms (53.08%) | Memory: 104.9 MB (53.00%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -9,43 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class NodeValue {
+    public:
+    int minNode, maxNode, sum;
+    NodeValue(int mini, int maxi, int s) {
+        minNode =mini;
+        maxNode =maxi;
+        sum =s;
+    }
+};
+
+
 class Solution {
 public:
 
-    int ans;
-
-    vector<int> solve(TreeNode* root){
-
-        if(!root){
-            return {1,INT_MAX,INT_MIN,0};
+    int ans =0;
+    NodeValue helper(TreeNode* root) {
+        if(!root) {
+            return NodeValue(INT_MAX, INT_MIN,0);
         }
+        auto left =helper(root->left);
+        auto right =helper(root->right);
 
-        vector<int> l = solve(root->left);
+        if(left.maxNode<root->val && root->val < right.minNode) {
+            int curr =left.sum+right.sum+root->val;
+            ans =max(ans, curr);
 
-        vector<int> r = solve(root->right);
-
-        if(l[0] && r[0] && root->val > l[2] && root->val < r[1]){
-
-            int sum = l[3] + r[3] + root->val;
-
-            ans = max(ans,sum);
-
-            int mn = min(root->val,l[1]);
-
-            int mx = max(root->val,r[2]);
-
-            return {1,mn,mx,sum};
+            return NodeValue(min(root->val, left.minNode), max(root->val, right.maxNode), curr);
         }
-
-        return {0,0,0,0};
+        return NodeValue(INT_MIN, INT_MAX, 0);
     }
 
+
+
     int maxSumBST(TreeNode* root) {
-
-        ans = 0;
-
-        solve(root);
-
+        //tc - o(n), sc - o(1)
+        helper(root);
         return ans;
     }
 };
