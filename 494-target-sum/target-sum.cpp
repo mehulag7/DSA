70ced786f65535ab437f059f6e7edb298a9a837e
mehulag7 <mehulagarwal2221@gmail.com>
2026-07-07 18:08:56 +0530

    Time: 25 ms (40.28%) | Memory: 39 MB (18.41%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        int sum=0;
         int n=nums.size();
-        vector<vector<long long>> dp(n,vector<long long>(2001,0));
-        dp[0][1000+nums[0]]=1;
-        dp[0][1000-nums[0]]+=1;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if(abs(target)>sum) return 0;
+        vector<vector<long long>> dp(n+1,vector<long long>(2*sum+1,0));
+        dp[0][sum-nums[0]]++;
+        dp[0][sum+nums[0]]++;
         for(int i=1;i<n;i++){
-            for(int j=0;j<=2000;j++){
-                if(j-nums[i]>=0){
-                    dp[i][j]+=dp[i-1][j-nums[i]];
-                }
-                if(j+nums[i]<=2000){
-                      dp[i][j]+=dp[i-1][j+nums[i]];
-                }
+            for(int j=0;j<=2*sum;j++){
+               if(j-nums[i]>=0) dp[i][j]=dp[i-1][j-nums[i]];
+               if(j+nums[i]<=2*sum) dp[i][j]+=dp[i-1][j+nums[i]];
             }
         }
-        return dp[n-1][target+1000];
+        return dp[n-1][target+sum];
     }
 };
