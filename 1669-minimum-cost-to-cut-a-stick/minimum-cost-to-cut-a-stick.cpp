2b9cedbeecc6f51bde6ec2874e5d18d806b848ea
mehulag7 <mehulagarwal2221@gmail.com>
2026-07-08 12:23:35 +0530

    Time: 540 ms (5.08%) | Memory: 15.5 MB (5.45%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
-        if (j - i <= 1) return 0;   // no cut possible
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int ans = 1e9;
-        for (int k = i + 1; k < j; k++) {
-            ans = min(ans,
-                solve(i, k, cuts, dp) +
-                solve(k, j, cuts, dp) +
-                cuts[j] - cuts[i]
-            );
-        }
-        return dp[i][j] = ans;
+    int solve(int st,int end,vector<int>& cuts,vector<vector<long long>> &dp){
+        if(st>end) return 0;
+      if(dp[st][end]!=-1) return dp[st][end];
+      if(st==end) return dp[st][end]=cuts[end+1]-cuts[end-1];
+      long long ans=1e9;
+      for(int i=st;i<=end;i++){
+       long long p=cuts[end+1]-cuts[st-1];
+       p+=solve(st,i-1,cuts,dp);
+       p+=solve(i+1,end,cuts,dp);
+       if(p<ans) ans=p;
+      }
+      return dp[st][end]=ans;
     }
-
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
-        sort(cuts.begin(), cuts.end());
-
-        int m = cuts.size();
-        vector<vector<int>> dp(m, vector<int>(m, 0));
-        //return solve(0, m - 1, cuts, dp);
-        for(int i=m-1;i>=0;i--){
-            for(int j=i+2;j<m;j++){
-                        int ans = 1e9;
-        for (int k = i + 1; k < j; k++) {
-            ans = min(ans,
-                dp[i][k]+dp[k][j]+
-                cuts[j] - cuts[i]
-            );
+        sort(cuts.begin(),cuts.end());
+        int p=cuts.size();
+        vector<vector<long long>> dp(p+1,vector<long long>(p+1,-1));
+        int ans=solve(1,p-2,cuts,dp);
+        for(int i=0;i<=p;i++){
+            for(int j=0;j<=p;j++) cout<<dp[i][j]<<" ";
+            cout<<endl;
         }
-         dp[i][j] = ans;
-            }
-        }
-        return dp[0][m-1];
+        return ans;
     }
 };
